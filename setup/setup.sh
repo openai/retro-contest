65668002b7710534ccfa2c742c959ef77d627e1c
Vicki Pfau <vickipfau@openai.com>
2017-12-06 14:20:19 -0800

    setup: Cleanup

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,9 +5,9 @@ if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker build -f Dockerfile.compo-env -t compo-env ..
-docker build -f Dockerfile.remote-env-bare -t remote-env:bare ..
-docker build -f Dockerfile.remote-env -t remote-env ..
+docker build -f compo-env.docker -t compo-env ..
+docker build -f remote-env-0.docker -t remote-env:bare ..
+docker build -f remote-env-1.docker -t remote-env ..
 docker volume create --driver local \
     --opt type=tmpfs \
     --opt device=tmpfs \
