c9228d5a1cc9b26b1903cd1821d0a5aa78ad8940
Vicki Pfau <vickipfau@openai.com>
2017-12-06 14:03:56 -0800

    setup: Import ROMs

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-SCRIPTDIR=`dirname $0`
-if [ -d $SCRIPTDIR/../.git ]; then
+cd `dirname $0`
+if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker build -f $SCRIPTDIR/Dockerfile.compo-env -t compo-env .
-docker build -f $SCRIPTDIR/Dockerfile.remote-env -t remote-env .
+docker build -f Dockerfile.compo-env -t compo-env ..
+docker build -f Dockerfile.remote-env-bare -t remote-env:bare ..
+docker build -f Dockerfile.remote-env -t remote-env ..
 docker volume create --driver local \
     --opt type=tmpfs \
     --opt device=tmpfs \
