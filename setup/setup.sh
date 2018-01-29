4ed1a38345a355834f53dc34424ba7db39978056
Vicki Pfau <vickipfau@openai.com>
2018-01-29 11:13:13 -0800

    setup: Update to TensorFlow 1.5

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,8 +6,8 @@ if [ -d ../.git ]; then
 fi
 
 docker build -f agent.docker -t agent ..
-docker build -f agent-tf.docker -t agent:tensorflow-1.4 ..
-docker build -f agent-pytorch.docker -t agent:pytorch-3.0 ..
+docker build -f agent-tf.docker -t agent:tensorflow ..
+docker build -f agent-pytorch.docker -t agent:pytorch ..
 docker build -f remote-env-0.docker -t remote-env:bare ..
 docker build -f remote-env-1.docker -t remote-env ..
 docker volume create --driver local \
