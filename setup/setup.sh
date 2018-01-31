b482306bc02f15e0d62c7ad02e9cf50478025cda
Vicki Pfau <vickipfau@openai.com>
2018-01-31 11:13:41 -0800

    setup: Use a prerelease gym 0.9.6 until it gets released

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,12 +5,8 @@ if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker build -f agent.docker -t agent ..
-docker build -f agent-tf.docker -t agent:tensorflow ..
-docker build -f agent-pytorch.docker -t agent:pytorch ..
-docker build -f remote-env-0.docker -t remote-env:bare ..
+docker build -f agent.docker -t agent .. && \
+docker build -f agent-tf.docker -t agent:tensorflow .. && \
+docker build -f agent-pytorch.docker -t agent:pytorch .. && \
+docker build -f remote-env-0.docker -t remote-env:bare .. && \
 docker build -f remote-env-1.docker -t remote-env ..
-docker volume create --driver local \
-    --opt type=tmpfs \
-    --opt device=tmpfs \
-    compo-tmp-vol
