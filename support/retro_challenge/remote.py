f8833641acb8b97e4b32c843cebf8a8be1f2a58b
Vicki Pfau <vickipfau@openai.com>
2018-02-02 17:59:54 -0800

    support: Add in-progress stats

--- support/retro_challenge/remote.py
+++ support/retro_challenge/remote.py
@@ -43,14 +43,20 @@ class StochasticFrameSkip(gym.Wrapper):
 
 
 class Monitor(gym.Wrapper):
-    def __init__(self, env, monitorfile):
+    def __init__(self, env, monitorfile, logfile=None):
         gym.Wrapper.__init__(self, env)
         self.file = open(monitorfile, 'w')
         self.csv = csv.DictWriter(self.file, ['r', 'l', 't'])
+        self.log = open(logfile, 'w')
+        self.logcsv = csv.DictWriter(self.log, ['l', 't'])
         self.episode_reward = 0
         self.episode_length = 0
+        self.total_length = 0
         self.start = None
         self.csv.writeheader()
+        self.file.flush()
+        self.logcsv.writeheader()
+        self.log.flush()
 
     def reset(self, **kwargs):
         if not self.start:
@@ -61,6 +67,7 @@ class Monitor(gym.Wrapper):
                 'l': self.episode_length,
                 't': time.time() - self.start
             })
+            self.file.flush()
         self.episode_length = 0
         self.episode_reward = 0
         return self.env.reset(**kwargs)
@@ -68,7 +75,14 @@ class Monitor(gym.Wrapper):
     def step(self, ac):
         ob, rew, done, info = self.env.step(ac)
         self.episode_length += 1
+        self.total_length += 1
         self.episode_reward += rew
+        if self.total_length % 1000 == 0:
+            self.logcsv.writerow({
+                'l': self.total_length,
+                't': time.time() - self.start
+            })
+            self.log.flush()
         return ob, rew, done, info
 
     def __del__(self):
@@ -85,7 +99,7 @@ def make(game, state, bk2dir=None, monitordir=None, discrete_actions=False, sock
     env = StochasticFrameSkip(env, n=4, stickprob=0.25)
     env = gym.wrappers.TimeLimit(env, max_episode_steps=4500)
     if monitordir:
-        env = Monitor(env, os.path.join(monitordir, 'monitor.csv'))
+        env = Monitor(env, os.path.join(monitordir, 'monitor.csv'), os.path.join(monitordir, 'log.csv'))
     env = grs.RemoteEnvWrapper(env, socketdir)
     return env
 
