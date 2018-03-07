3ef84a7bfb8b6535095148e8c29bc8132ae67c6e
Vicki Pfau <vickipfau@openai.com>
2018-03-07 13:00:48 -0800

    setup: Take Docker images off of quay

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,13 +5,10 @@ if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker pull -a quay.io/openai/retro-agent
-docker pull -a quay.io/openai/retro-env
-docker build -f agent.docker -t quay.io/openai/retro-agent --cache-from quay.io/openai/retro-agent .. && \
-docker build -f agent-tf.docker -t quay.io/openai/retro-agent:tensorflow --cache-from quay.io/openai/retro-agent:tensorflow .. && \
-docker build -f agent-pytorch.docker -t quay.io/openai/retro-agent:pytorch --cache-from quay.io/openai/retro-agent:pytorch .. && \
-docker build -f remote-env-0.docker -t quay.io/openai/retro-env --cache-from quay.io/openai/retro-env .. && \
-docker build -f remote-env-1.docker -t quay.io/openai/retro-env-full --cache-from quay.io/openai/retro-env-full ..
-docker tag quay.io/openai/retro-agent agent
-docker tag quay.io/openai/retro-env remote-env:bare
-docker tag quay.io/openai/retro-env-full remote-env
+docker build -f agent.docker -t openai/retro-agent --cache-from openai/retro-agent .. && \
+docker build -f agent-tf.docker -t openai/retro-agent:tensorflow --cache-from openai/retro-agent:tensorflow .. && \
+docker build -f agent-pytorch.docker -t openai/retro-agent:pytorch --cache-from openai/retro-agent:pytorch .. && \
+docker build -f remote-env-0.docker -t openai/retro-env --cache-from openai/retro-env .. && \
+docker build -f remote-env-1.docker -t remote-env --cache-from openai/retro-env ..
+docker tag openai/retro-agent agent
+docker tag openai/retro-env remote-env:bare
