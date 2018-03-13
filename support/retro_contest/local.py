535ae30eeb5b9adfa2a83c27e774c913edd0b376
Vicki Pfau <vickipfau@openai.com>
2018-03-13 16:51:24 -0700

    support: Fall back to default scenario if contest.json is not found

--- support/retro_contest/local.py
+++ support/retro_contest/local.py
@@ -7,7 +7,10 @@ def make(game, state, discrete_actions=False, bk2dir=None):
     use_restricted_actions = retro.ACTIONS_FILTERED
     if discrete_actions:
         use_restricted_actions = retro.ACTIONS_DISCRETE
-    env = retro.make(game, state, scenario='contest', use_restricted_actions=use_restricted_actions)
+    try:
+        env = retro.make(game, state, scenario='contest', use_restricted_actions=use_restricted_actions)
+    except Exception:
+        env = retro.make(game, state, use_restricted_actions=use_restricted_actions)
     if bk2dir:
         env.auto_record(bk2dir)
     env = retro_contest.StochasticFrameSkip(env, n=4, stickprob=0.25)
