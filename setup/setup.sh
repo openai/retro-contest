733627ce84c04474ae9978b42aebbc8cad7f758f
Vicki Pfau <vickipfau@openai.com>
2018-03-13 12:29:16 -0700

    setup: Make TensorFlow agent the default image

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,10 +5,11 @@ if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker build -f agent.docker -t openai/retro-agent --cache-from openai/retro-agent .. && \
+docker build -f agent.docker -t openai/retro-agent:bare --cache-from openai/retro-agent:bare .. && \
 docker build -f agent-tf.docker -t openai/retro-agent:tensorflow --cache-from openai/retro-agent:tensorflow .. && \
 docker build -f agent-pytorch.docker -t openai/retro-agent:pytorch --cache-from openai/retro-agent:pytorch .. && \
 docker build -f remote-env-0.docker -t openai/retro-env --cache-from openai/retro-env .. && \
 docker build -f remote-env-1.docker -t remote-env --cache-from openai/retro-env ..
-docker tag openai/retro-agent agent
+docker tag openai/retro-agent:tensorflow openai/retro-agent:latest
+docker tag openai/retro-agent:latest agent
 docker tag openai/retro-env remote-env:bare
