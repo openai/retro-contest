51f1ca70034240f9f027d9d2964509b893b01388
Vicki Pfau <vickipfau@openai.com>
2018-03-26 14:38:39 -0700

    setup: Support TensorFlow 1.4 and 1.6

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,11 +5,14 @@ if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker build -f agent.docker -t openai/retro-agent:bare --cache-from openai/retro-agent:bare .. && \
-docker build -f agent-tf.docker -t openai/retro-agent:tensorflow --cache-from openai/retro-agent:tensorflow .. && \
+docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda8 --build-arg CUDA=8.0-cudnn6 --cache-from openai/retro-agent:bare-cuda8 .. && \
+docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda9 --build-arg CUDA=9.0-cudnn7 --cache-from openai/retro-agent:bare-cuda9 .. && \
+docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.4 --build-arg CUDA=8 --build-arg TF=1.4.1 --cache-from openai/retro-agent:tensorflow-1.4 .. && \
+docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.6 --build-arg CUDA=9 --build-arg TF=1.6.0 --cache-from openai/retro-agent:tensorflow-1.6 .. && \
 docker build -f agent-pytorch.docker -t openai/retro-agent:pytorch --cache-from openai/retro-agent:pytorch .. && \
 docker build -f remote-env-0.docker -t openai/retro-env --cache-from openai/retro-env .. && \
-docker build -f remote-env-1.docker -t remote-env --cache-from openai/retro-env ..
+docker build -f remote-env-1.docker -t remote-env ..
+docker tag openai/retro-agent:tensorflow-1.6 openai/retro-agent:tensorflow
 docker tag openai/retro-agent:tensorflow openai/retro-agent:latest
 docker tag openai/retro-agent:latest agent
 docker tag openai/retro-env remote-env:bare
