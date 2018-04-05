bf1846b7ced485198351901d03e1a9f11a659a35
Vicki Pfau <vickipfau@openai.com>
2018-04-05 09:17:38 -0700

    setup: Update to TensorFlow 1.7

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@ fi
 docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda8 --build-arg CUDA=8.0-cudnn6 --cache-from openai/retro-agent:bare-cuda8 .. && \
 docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda9 --build-arg CUDA=9.0-cudnn7 --cache-from openai/retro-agent:bare-cuda9 .. && \
 docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.4 --build-arg CUDA=8 --build-arg TF=1.4.1 --cache-from openai/retro-agent:tensorflow-1.4 .. && \
-docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.6 --build-arg CUDA=9 --build-arg TF=1.6.0 --cache-from openai/retro-agent:tensorflow-1.6 .. && \
+docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.7 --build-arg CUDA=9 --build-arg TF=1.7.0 --cache-from openai/retro-agent:tensorflow-1.7 .. && \
 docker build -f agent-pytorch.docker -t openai/retro-agent:pytorch --cache-from openai/retro-agent:pytorch .. && \
 docker build -f remote-env-0.docker -t openai/retro-env --cache-from openai/retro-env .. && \
 docker build -f remote-env-1.docker -t remote-env ..
