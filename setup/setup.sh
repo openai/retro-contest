cf46d40beb3270cb7f4bf91d784e7bfb6e19e0d8
Vicki Pfau <vickipfau@openai.com>
2018-04-09 10:18:13 -0700

    setup: Build off of specific versions of cudnn (fixes #1)

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,8 +5,8 @@ if [ -d ../.git ]; then
     git submodule update --init --recursive
 fi
 
-docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda8 --build-arg CUDA=8.0-cudnn6 --cache-from openai/retro-agent:bare-cuda8 .. && \
-docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda9 --build-arg CUDA=9.0-cudnn7 --cache-from openai/retro-agent:bare-cuda9 .. && \
+docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda8 --build-arg CUDA=8.0 --build-arg CUDNN=6 --cache-from openai/retro-agent:bare-cuda8 .. && \
+docker build --pull -f agent.docker -t openai/retro-agent:bare-cuda9 --build-arg CUDA=9.0 --build-arg CUDNN=7 --cache-from openai/retro-agent:bare-cuda9 .. && \
 docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.4 --build-arg CUDA=8 --build-arg TF=1.4.1 --cache-from openai/retro-agent:tensorflow-1.4 .. && \
 docker build -f agent-tf.docker -t openai/retro-agent:tensorflow-1.7 --build-arg CUDA=9 --build-arg TF=1.7.0 --cache-from openai/retro-agent:tensorflow-1.7 .. && \
 docker build -f agent-pytorch.docker -t openai/retro-agent:pytorch --cache-from openai/retro-agent:pytorch .. && \
